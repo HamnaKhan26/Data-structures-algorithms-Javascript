14e280f42b1a7c8cd92640b2954127a30202235c
HamnaKhan26 <hamnarana.faisal26@gmail.com>
2025-12-27 12:55:52 -0500

    spiral traverse

--- data-structures/matrixArrays/traversal.js
+++ data-structures/matrixArrays/traversal.js
@@ -28,3 +28,37 @@ function traverseColumnWise(matrix) {
 
 traverseColumnWise(matrix);
 
+// Spiral traversal
+function traverseSpiral(matrix) {
+    let top = 0;  //starting row index
+    let bottom = matrix.length - 1;  //ending row index
+    let left = 0;  //starting column index
+    let right = matrix[0].length - 1; //ending column index
+
+    if (matrix.length === 0) return [];
+
+    while (top <= bottom && left <= right) {
+        for (let col = left; col <= right; col++) {  //first row
+            console.log(matrix[top][col]);
+        }
+        top++;
+        for (let row = top; row <= bottom; row++) {  //right column
+            console.log(matrix[row][right]);
+        }
+        right--;
+        if (top <= bottom) {  //last row
+            for (let col = right; col >= left; col--) {
+                console.log(matrix[bottom][col]);
+            }
+            bottom--;
+        }
+        if (left <= right) { //left column
+            for (let row = bottom; row >= top; row--) {
+                console.log(matrix[row][left]);
+            }
+            left++;
+        }
+    }
+
+}
+traverseSpiral(matrix);
