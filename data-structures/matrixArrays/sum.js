6f346a7e67d58270e9a6d3b87a05b4afdff2954d
HamnaKhan26 <hamnarana.faisal26@gmail.com>
2025-12-28 12:39:09 -0500

    matrix row/col sum

--- data-structures/matrixArrays/sum.js
+++ data-structures/matrixArrays/sum.js
@@ -28,3 +28,67 @@ var diagonalSum = function(mat) {
 
 
 diagonalSum(matrix);
+
+
+//find sum of each row
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var rowSums = function(matrix) {
+    const result = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < matrix[i].length; j++) {
+            sum += matrix[i][j];
+        }
+        result.push(sum);
+    }
+
+    return result;
+};
+rowSums(matrix);
+
+
+//find row with max sum
+/**
+ * @param {number[][]} matrix
+ * @return {number}  // index of row with max sum
+ */
+var maxRowSum = function(matrix) {
+    let maxSum = -Infinity;
+    let maxRowIndex = -1;
+
+    for (let i = 0; i < matrix.length; i++) {
+        let sum = 0;
+
+        for (let j = 0; j < matrix[i].length; j++) {
+            sum += matrix[i][j];
+        }
+
+        if (sum > maxSum) {
+            maxSum = sum;
+            maxRowIndex = i;
+        }
+    }
+
+    return maxRowIndex;
+};
+maxRowSum(matrix);
+
+//find sum of each column
+var columnSums = function(matrix) {
+    const result = [];
+    const cols = matrix[0].length;
+
+    for (let j = 0; j < cols; j++) {
+        let sum = 0;
+        for (let i = 0; i < matrix.length; i++) {
+            sum += matrix[i][j];
+        }
+        result.push(sum);
+    }
+
+    return result;
+};
