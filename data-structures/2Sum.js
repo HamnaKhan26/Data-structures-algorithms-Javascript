7cb924fa7a123e6ccd525ff2077a8935a0fb7c4b
HamnaKhan26 <hamnarana.faisal26@gmail.com>
2026-02-09 13:08:34 -0500

    practice 2 sum less than K problem

--- data-structures/2Sum.js
+++ data-structures/2Sum.js
@@ -13,3 +13,26 @@ function twoSum(arr, target) {
 }
 
 console.log(twoSum(array, 4)); // Output: [1, 3] because array[1] + array[3] = 5 + (-1) = 4
+
+
+function twoSumLessThanK(nums, k) {
+    nums.sort((a, b) => a - b); // Sort ascending
+    let left = 0;
+    let right = nums.length - 1;
+    let maxSum = -1;
+
+    while (left < right) {
+        let sum = nums[left] + nums[right];
+        if (sum < k) {
+            maxSum = Math.max(maxSum, sum);
+            left++; // try a bigger sum
+        } else {
+            right--; // sum too big, decrease
+        }
+    }
+
+    return maxSum;
+}
+
+// Example
+console.log(twoSumLessThanK([34,23,1,24,75,33,54,8], 60)); // 58
